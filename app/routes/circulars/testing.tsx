4761d9344ec27687b2627eee11bd66bc5156be95
tylerbarna <tylerpbarna@gmail.com>
2023-07-27 18:09:07 -0500

    date picker in-progress
    date range picker element is coming along, now has an example of buttons for fuzzy dates along with a button that changes the navigation to be the absolute date range picker. Should implement the setting of the value for the button.

--- app/routes/circulars/testing.tsx
+++ app/routes/circulars/testing.tsx
@@ -6,102 +6,125 @@
  * SPDX-License-Identifier: NASA-1.3
  */
 import type { DataFunctionArgs } from '@remix-run/node'
-import {
-  Form,
-  Link,
-  useActionData,
-  useLoaderData,
-  useSearchParams,
-  useSubmit,
-} from '@remix-run/react'
+import { Form, useSubmit } from '@remix-run/react'
 import {
   Button,
   ButtonGroup,
-  Icon,
+  DateRangePicker,
+  Dropdown,
   Label,
-  Menu,
-  Select,
-  TextInput,
 } from '@trussworks/react-uswds'
 import classNames from 'classnames'
 import { useState } from 'react'
+import React from 'react'
 
 import { circularRedirect, put, search } from './circulars.server'
+import DetailsDropdownButton from '~/components/DetailsDropdownButton'
+import DetailsDropdownContent from '~/components/DetailsDropdownContent'
 import Hint from '~/components/Hint'
 import { Popup, hideableElement } from '~/components/Popup'
-import { usePagination } from '~/lib/pagination'
-import { getFormDataString } from '~/lib/utils'
 
 import searchImg from 'app/theme/img/usa-icons-bg/search--white.svg'
 
-function toggleVisibility(target: string) {
-  const element = document.getElementById(target)
-  if (element) {
-    element.style.visibility =
-      element.style.visibility === 'hidden' ? 'visible' : 'hidden'
-  }
-}
-
 export default function () {
-  var expanded = 'none'
   const [expandedState] = useState(true)
-  const onChangeCaptureHandler = (e: any) => {
-    console.log('value changed to', e.target.value)
-    const searchParams = new URLSearchParams()
-    searchParams.set('query', e.target.value)
-    if (e.target.value === 'dateRange') {
-      console.log('set a custom dateRange')
-    } else {
-      console.log('setting a fuzzy date Range')
-      //expanded = !expanded
-      //expanded = 'block'
-      toggleVisibility('expandingMenu')
-    }
-  }
+  const [showContent, setShowContent] = useState(false)
+  const [showDateRange, setShowDateRange] = useState(false)
+  let displayText = 'Date Range'
+  const submit = useSubmit()
   return (
     <>
       <div> you are in testing.tsx </div>
-      {/* <Button
-        type="button"
-        className="height-4 padding-top-0 padding-bottom-0"
-        onClick={() => {
-          expanded = !expanded
-          console.log('clicked')
-          console.log(expanded)
-        }}
-        children={undefined}
-      ></Button> */}
-      <select
-        className="usa-button"
-        name="options"
-        id="options"
-        defaultValue={undefined}
-        onChangeCapture={onChangeCaptureHandler}
+
+      <DetailsDropdownButton
+        onClick={() => setShowContent(!showContent)}
+        outline
       >
-        {/* <img
-            className="usa-button__icon usa-button__icon--size-2"
-            src={searchImg}
-            alt="Search"
-          /> */}
-        <option value=""></option>
-        <option value="lastHour">Last Hour</option>
-        <option value="lastDay">Last Day</option>
-        <option value="lastWeek">Last Week</option>
-        <option value="lastMonth">Last Month</option>
-        <option value="ytd">Year To Date</option>
-        <option
-          value="dateRange"
-          onSelect={
-            (e) => console.log('clicked')
-            // console.log(clickState)
-          }
-        >
-          Custom Range
-        </option>
-      </select>
-      <p className="usa-menu" id="expandingMenu">
-        t
-      </p>
+        {displayText}
+      </DetailsDropdownButton>
+      {showContent && (
+        <DetailsDropdownContent>
+          {!showDateRange && (
+            <>
+              <Button
+                type="button"
+                className="usa-button usa-button--unstyled"
+                onClick={() => {
+                  displayText = 'button1'
+                }}
+              >
+                button1
+              </Button>
+              <Button type="button" className="usa-button usa-button--unstyled">
+                button2
+              </Button>
+            </>
+          )}
+          {!showDateRange && (
+            <Button
+              type="button"
+              className="usa-button"
+              onClick={() => setShowDateRange(!showDateRange)}
+            >
+              expand date range
+            </Button>
+          )}
+          {showDateRange && (
+            <>
+              <Button
+                type="button"
+                className="usa-button"
+                onClick={() => setShowDateRange(!showDateRange)}
+              >
+                hide
+              </Button>
+              <DateRangePicker
+                startDateHint="dd/mm/yyyy"
+                startDateLabel="Start Date"
+                startDatePickerProps={{
+                  id: 'event-date-start',
+                  name: 'event-date-start',
+                  defaultValue: 'startDate',
+                  // onChange: (value) => {
+                  //   if (value) {
+                  //     let params = new URLSearchParams(location.search)
+                  //     params.set('startDate', value)
+                  //     submit(params, {
+                  //       method: 'get',
+                  //       action: '/circulars',
+                  //     })
+                  //   }
+                  // },
+                }}
+                endDateHint="dd/mm/yyyy"
+                endDateLabel="End Date"
+                endDatePickerProps={{
+                  id: 'event-date-end',
+                  name: 'event-date-end',
+                  defaultValue: 'endDate',
+                  // onChange: (value) => {
+                  //   if (value) {
+                  //     let params = new URLSearchParams(location.search)
+                  //     params.set('endDate', value)
+                  //     submit(params, {
+                  //       method: 'get',
+                  //       action: '/circulars',
+                  //     })
+                  //   }
+                  // },
+                }}
+              />
+              <Button
+                type="button"
+                className="usa-button margin-top-2"
+                onClick={() => submit('test')}
+              >
+                submit dates
+              </Button>
+            </>
+          )}
+        </DetailsDropdownContent>
+      )}
     </>
   )
 }
