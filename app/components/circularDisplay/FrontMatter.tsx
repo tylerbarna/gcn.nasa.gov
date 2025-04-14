61ac71f2eed8a8643978c28ab423905904f049c7
Tyler Barna <tylerpbarna@gmail.com>
2025-04-14 14:02:50 -0500

    add event to heaader

--- app/components/circularDisplay/FrontMatter.tsx
+++ app/components/circularDisplay/FrontMatter.tsx
@@ -37,6 +37,7 @@ function FrontMatterItem({
 
 export function FrontMatter({
   subject,
+  eventId,
   createdOn,
   submitter,
   submittedHow,
@@ -45,6 +46,7 @@ export function FrontMatter({
 }: Pick<
   Circular,
   | 'subject'
+  | 'eventId'
   | 'createdOn'
   | 'submitter'
   | 'submittedHow'
@@ -54,6 +56,7 @@ export function FrontMatter({
   return (
     <>
       <FrontMatterItem label="Subject">{subject}</FrontMatterItem>
+      {eventId && <FrontMatterItem label="Event">{eventId}</FrontMatterItem>}
       <FrontMatterItem label="Date">
         {formatDateISO(createdOn)}{' '}
         <small className="text-base-light">
