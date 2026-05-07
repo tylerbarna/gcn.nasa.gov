b551cbc455fa859a9775c5741d2141abb5932c40
tylerbarna <tylerpbarna@gmail.com>
2026-05-07 16:13:05 -0500

    Create tests for parseEventTypeFromSubject

--- __tests__/circulars.ts
+++ __tests__/circulars.ts
@@ -13,6 +13,7 @@ import {
   formatAuthor,
   formatCircularText,
   parseEventFromSubject,
+  parseEventTypeFromSubject,
   subjectIsValid,
 } from '../app/routes/circulars/circulars.lib'
 
@@ -1344,5 +1345,69 @@ describe('subjectIsValid', () => {
   })
   test('returns true on valid subject', () => {
     expect(subjectIsValid('GRB ABC000123')).toBe(true)
+  })
+})
+
+// Tests the event type tagging with eventTypeMatchers and parseEventTypeFromSubject
+describe('parseEventTypeFromSubject', () => {
+  describe('Retraction', () => {
+    test('handles retraction subject', () => {
+      const retractionSubject = 'LIGO/Virgo S190405ar: Retraction of a GW binary merger candidate'
+      expect(parseEventTypeFromSubject(retractionSubject)).toBe('Retraction')
+    })
+  })
+  describe('GRB', () => {
+    test('handles GRB subject', () => {
+      const grbSubject = 'GRB ABC000123: Swift UVOT follow-up'
+      expect(parseEventTypeFromSubject(grbSubject)).toBe('GRB')
+    })
+  })
+  describe('GW', () => {
+    test('handles GW subject', () => {
+      const gwSubject = 'LIGO/Virgo/KAGRA S240601co: Updated Sky localization'
+      expect(parseEventTypeFromSubject(gwSubject)).toBe('GW')
+    })
+  })
+  describe('SGR', () => {
+    test('handles SGR subject', () => {
+      const sgrSubject = 'SGR1627-41, Refined IPN annulus location'
+      expect(parseEventTypeFromSubject(sgrSubject)).toBe('SGR')
+    })
+  })
+  describe('FRB', () => {
+    test('handles FRB subject', () => {
+      const frbSubject = 'FRB 20250316A: FTW optical observations'
+      expect(parseEventTypeFromSubject(frbSubject)).toBe('FRB')
+    })
+  })
+  describe('SN', () => {
+    test('handles SN subject', () => {
+      const snSubject = 'XRF 080109 / SN 2008D: UKIRT JHK imaging'
+      expect(parseEventTypeFromSubject(snSubject)).toBe('SN')
+    })
+  })
+  describe('Neutrino', () => {
+    test('handles Neutrino subject', () => {
+      const neutrinoSubject = 'IceCube-200926A: No Candidate Counterparts from the Zwicky Transient Facility'
+      expect(parseEventTypeFromSubject(neutrinoSubject)).toBe('Neutrino')
+    })
+  })
+  describe('X-Ray Transient', () => {
+    test('handles X-Ray Transient subject', () => {
+      const xraySubject = 'EP240913a: NOT optical upper limits'
+      expect(parseEventTypeFromSubject(xraySubject)).toBe('X-Ray Transient')
+    })
+  })
+  describe('Afterglow', () => {
+    test('handles Afterglow subject', () => {
+      const afterglowSubject = 'iPTF14yb: iPTF Discovery of an Optical Afterglow-like Transient'
+      expect(parseEventTypeFromSubject(afterglowSubject)).toBe('Afterglow')
+    })
+  })
+  describe('Optical Transient', () => {
+    test('handles Optical Transient subject', () => {
+      const otSubject = 'AT2023avj ZTF Candidate: Assy optical observations'
+      expect(parseEventTypeFromSubject(otSubject)).toBe('Optical Transient')
+    })
   })
 })
